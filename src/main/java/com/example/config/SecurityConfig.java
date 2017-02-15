51cc98a925a715d13496926a4166fb817056471d
java <java>
2017-02-15 14:03:19 +0900

    cookie

---
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.example.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+//@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+				.withUser("admin")
+				.password("1234")
+				.roles("ADMIN");
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+		.authorizeRequests()
+			.antMatchers("/cookie/**").authenticated()
+			.antMatchers("/*").permitAll()
+			.and()
+		.formLogin()
+			.and()
+		.httpBasic();
+	}
+
+}
